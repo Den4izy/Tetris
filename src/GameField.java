f9c266085779c4ace6f1b4c172ca89f7a20f36b0
Den4izy <kawoonne@gmail.com>
2019-01-29 22:55:16 +0200

    29.01.19

--- src/GameField.java
+++ src/GameField.java
@@ -1,4 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameField extends JPanel {
+
+    private Image dot;
+    private Timer timer;
+
+    public GameField() {
+        setBackground(Color.BLACK);
+        loadImages();
+    }
+    //загрузка картинок
+    public void loadImages(){
+        ImageIcon img = new ImageIcon("dot.png");
+        dot = img.getImage();
+    }
 }
